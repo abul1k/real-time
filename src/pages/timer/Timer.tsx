35cc260d7c86a37860c20f58fda2b0754e942ee6
Abdulaziz <abulacoder@gmail.com>
2023-12-06 23:26:20 +0500

    checking the abdulazyz branch

--- src/pages/timer/Timer.tsx
+++ src/pages/timer/Timer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Timer = () => {
   return (
     <div className="world-clock">
-      <h1 className="centered-text">Coming soon</h1>
+      <h1 className="centered-text">Coming soon (timer)</h1>
     </div>
   );
 };
